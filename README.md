cdc1f0f340ab81292bf66c82e2d9e3b928ecd55c
D_first <dntk.daisei@gmail.com>
2021-11-07 16:25:04 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# QuickNotes
+
+This is a Foundry VTT mod.
+Use the journal to take quick notes from the chat content.
---
+++ scripts/quick-notes.js
@@ -0,0 +1,52 @@
+class QuickNotes {
+    static ID = 'quick-notes';
+
+    static FLAGS = {
+        NOTES: 'quicknotes'
+    }
+}
+
+class QuickNotesData {
+    static getQuickNotesJournalEntry() {
+        const id = game.user?.getFlag(QuickNotes.ID, QuickNotes.FLAGS.NOTES);
+        return game.journal.get(id);
+    }
+
+    static async createQuickNotesJournal() {
+        const journalEntryName = game.i18n.localize('QUICK-NOTES.journal-entry-name');
+        const newJournalEntry = await JournalEntry.create(
+            {
+                name: game.user.name + journalEntryName,
+            },
+            { renderSheet: false, activate: false }
+        );
+
+        await game.user?.setFlag(QuickNotes.ID, QuickNotes.FLAGS.NOTES, newJournalEntry.id);
+
+        return newJournalEntry
+    }
+
+    static async takeNotes(journalEntry, newContent) {
+        const oldContent = journalEntry.data.content;
+        await journalEntry.update({
+            content: oldContent + newContent + '<br><br>'
+        });
+    }
+}
+
+Hooks.on('renderChatMessage', (message, html, data) => {
+    const tooltip = game.i18n.localize('QUICK-NOTES.button-title');
+    const takeNotesButton = $(`<a class="" style=""><i class="fas fa-feather" title="${tooltip}"></i></button>`);
+
+    html.find('.message-metadata').append(takeNotesButton);
+
+    takeNotesButton.click(async e => {
+        let quickNotesJournalEntry = QuickNotesData.getQuickNotesJournalEntry()
+        if (quickNotesJournalEntry == null) {
+            quickNotesJournalEntry = await QuickNotesData.createQuickNotesJournal();
+        }
+
+        QuickNotesData.takeNotes(quickNotesJournalEntry, message.export());
+        takeNotesButton.css('color', 'Green');
+    });
+});
